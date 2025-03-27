4bdc6c3365709ffe271f7a63d59cb74be339b029
AdarsHH30 <adarshhegde380@gmail.com>
2025-03-27 21:16:11 +0530

    test9

--- backend/settings.py
+++ backend/settings.py
@@ -62,11 +62,15 @@ CHANNEL_LAYERS = {
     },
 }
 
-# Middleware (simplified)
+
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 # URLs and templates
@@ -79,6 +83,8 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
